3892b3b7ee7a9bfee12df689fca0f3a8bcb0e8e4
Naviamold1 <89490126+Naviamold1@users.noreply.github.com>
2022-09-30 20:46:48 +0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,48 @@
+<h1 align="center">Welcome to searcher-ge 👋</h1>
+<p>
+  <a href="https://opensource.org/licenses/MIT" target="_blank">
+    <img alt="License: MIT" src="https://img.shields.io/badge/License-MIT-yellow.svg" />
+  </a>
+  <a href="https://twitter.com/naviamold" target="_blank">
+    <img alt="Twitter: naviamold" src="https://img.shields.io/twitter/follow/naviamold.svg?style=social" />
+  </a>
+</p>
+
+> Search multiple Georgian Websites at once
+
+## Install
+
+```sh
+git clone https://github.com/Naviamold1/searcher-ge.git
+cd searcher-ge
+pip install -r requirements.txt
+```
+
+## Usage
+
+```sh
+python main.py
+```
+
+## Author
+
+👤 **Naviamold**
+
+* Twitter: [@naviamold](https://twitter.com/naviamold)
+* Github: [@Naviamold1](https://github.com/Naviamold1)
+
+## 🤝 Contributing
+
+Contributions, issues and feature requests are welcome!<br />Feel free to check [issues page](https://github.com/Naviamold1/searcher-ge/issues). 
+
+## Show your support
+
+Give a ⭐️ if this project helped you!
+
+## 📝 License
+
+Copyright © 2022 [Naviamold](https://github.com/Naviamold1).<br />
+This project is [MIT](https://opensource.org/licenses/MIT) licensed.
+
+***
+_This README was generated with ❤️ by [readme-md-generator](https://github.com/kefranabg/readme-md-generator)_
